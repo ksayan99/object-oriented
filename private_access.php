26ea70b146d5a0b828d30c9636ef2f5583a1e838
ksayan99 <sayan@hih7.com>
2022-04-04 17:26:03 +0530

    "oops fixing with new file"

--- private_access.php
+++ private_access.php
@@ -3,8 +3,8 @@
 class base{
     private $name;
     public function show(){
-    	$this->$name = 'Sayan';
-        echo $this->$name;
+    	$this->name = 'Sayan';
+        echo $this->name;
     }
 }
 $obj = new base();
